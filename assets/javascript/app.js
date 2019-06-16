a8638fb1da94f83aafb043c2cf2d3a3c52fcb1a2
Marisa Ramon <mer0522@gmail.com>
2019-06-16 14:03:01 -0500

    complete JS logic and html framework

---
+++ assets/javascript/app.js
@@ -0,0 +1,113 @@
+ // VARIABLES
+    // ==========================================================================
+
+    // The array of questions for quiz game.
+    var questions = [
+        { q: "Color of the Sky?", a: ["blue","green", "yellow", "white"],correctAnswer: "blue" },
+        { q: "Color of the Sky?", a: ["blue","green", "yellow", "white"],correctAnswer: "blue" },
+        { q: "Color of the Sky?", a: ["blue","green", "yellow", "white"],correctAnswer: "blue" },
+        { q: "Color of the Sky?", a: ["blue","green", "yellow", "white"],correctAnswer: "blue" },
+        { q: "Color of the Sky?", a: ["blue","green", "yellow", "white"],correctAnswer: "blue" }
+      ];
+  
+      // start the game with a score of 0.
+      var correct = 0;
+      var incorrect = 0;
+      var counter = 120;
+
+  
+      // FUNCTIONS
+      // ==============================================================================
+      function startGame() {
+        timer = setInterval(countDown, 1000);
+        $("#sub-wrapper").prepend("<h2>Time Remaining: <span id='counter-value'>120</span> Seconds</h2>");
+        $("#start").remove();
+        for(var i=0; i<questions.length; i++){
+            $("#quiz-area").append("<h2>" +questions[i].q +"</h2>");
+            for(var j=0; j<questions[i].a.length; j++){
+                console.log("nested for loop")
+                $("#quiz-area").append("<input type='radio' name='question-" + i +
+                "' value='" + questions[i].a[j] + "''>" + questions[i].a[j])
+            }
+        }
+        $("#quiz-area").append("<button id='submit'>Submit</button>");
+      }
+      function countDown(){
+          counter--;
+          $("#counter-value").html(counter);
+          if(counter === 0){
+              console.log("TIME IS UP");
+              checkResults();
+          }
+      }
+      function checkResults(){
+          $.each($("input[name='question-0']:checked"),function(){
+              if($(this).val() === questions[0].correctAnswer){
+                  correct++;
+              }else{
+                  incorrect++;
+              }
+
+          });
+          $.each($("input[name='question-1']:checked"),function(){
+            if($(this).val() === questions[1].correctAnswer){
+                correct++;
+            }else{
+                incorrect++;
+            }
+
+        });
+        $.each($("input[name='question-2']:checked"),function(){
+            if($(this).val() === questions[2].correctAnswer){
+                correct++;
+            }else{
+                incorrect++;
+            }
+
+        });
+        $.each($("input[name='question-3']:checked"),function(){
+            if($(this).val() === questions[3].correctAnswer){
+                correct++;
+            }else{
+                incorrect++;
+            }
+
+        });
+        $.each($("input[name='question-4']:checked"),function(){
+            if($(this).val() === questions[4].correctAnswer){
+                correct++;
+            }else{
+                incorrect++;
+            }
+
+        });
+        showResults();
+
+      }
+      function showResults(){
+          // clear interval 
+          clearInterval(timer);
+          $('#submit').remove();
+          $('#sub-wrapper h2').remove();
+          // display correct and incorrect answers
+          $("#quiz-area").html("<h2>Results Displayed Here</h2>");
+          $("#quiz-area").append("<h3>Correct Answers: "+ correct +"</h3>");
+          $("#quiz-area").append("<h3>Incorrect Answers: "+ incorrect +"</h3>");
+          $("#quiz-area").append("<h3>Unanswered: " + (questions.length - (correct + incorrect)) + "</h3>");
+          if(correct === questions.length) {
+            alert("You scored 100%!");
+          } else {
+              alert("You're a loser");
+          }
+          
+      }
+
+      // MAIN PROCESS
+    // ==============================================================================
+$(document).on("click", "#start", function(){
+    startGame();
+})
+
+$(document).on("click", "#submit", function () {
+    checkResults();
+})
